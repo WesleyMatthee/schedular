0e5286bc51ccc301cc07d614c8e6ed86c2132a9c
Wesley Matthee <wrmatthee@gmail.com>
2023-01-11 18:13:42 -0500

    Add components Show, Status, Confirm, Error

--- stories/index.js
+++ stories/index.js
@@ -9,10 +9,14 @@ import InterviewerListItem from 'components/InterviewerListItem';
 import DayListItem from 'components/DayListItem';
 import DayList from 'components/DayList';
 import Button from "components/Button";
-import InterviewerList from 'components/InterviewerList'
+import InterviewerList from 'components/InterviewerList';
 import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header.js";
 import Empty from "components/Appointment/Empty.js";
+import Show from "components/Appointment/Show.js";
+import Confirm from "components/Appointment/Confirm.js";
+import Status from "components/Appointment/Status.js";
+import Error from "components/Appointment/Error.js";
 
 
 
@@ -46,7 +50,7 @@ storiesOf("DayListItem", module) //Initiates Storybook and registers our DayList
   ));
 
 
-  //DAYS ARRAY_____________
+//DAYS ARRAY_____________
 const days = [
   {
     id: 1,
@@ -82,7 +86,7 @@ storiesOf("DayList", module)
   ));
 
 
-  //INTERVIEWER LIST ITEM________________
+//INTERVIEWER LIST ITEM________________
 const interviewer = {
   id: 1,
   name: "Sylvia Palmer",
@@ -116,7 +120,7 @@ storiesOf("InterviewerListItem", module)
     />
   ));
 
-  //INTERVIEWER LIST_____________
+//INTERVIEWER LIST_____________
 const interviewers = [
   { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
   { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
@@ -147,7 +151,7 @@ storiesOf("InterviewerList", module)
     />
   ));
 
-  //APOINTMENT___________
+//APOINTMENT___________
 storiesOf("Appointment", module)
   .addParameters({
     backgrounds: [{ name: "white", value: "#fff", default: true }]
@@ -157,4 +161,24 @@ storiesOf("Appointment", module)
   .add("Header", () => <Header time="12pm" />)
   .add("Empty", () => <Empty onAdd={action("onAdd")} />)
 
- 
+  .add("Show", () => <Show
+  student="Lydia Miller-Jones"
+  interviewer={interviewers[2]}
+  onEdit={action("onEdit")}
+  onDelete={action("onDelete")}
+  />)
+
+  .add("Confirm", () => <Confirm
+    message={"Delete the appointment?"}
+    onConfirm={action("onConfirm")}
+    onCancel={action("onCancel")}
+  />)
+
+  .add("Status", () => <Status
+    message={"Deleteing"}
+  />)
+
+  .add("Error", () => <Error
+    message={"Could not delete appointment."}
+    onClose={action("onClose")}
+  />)
