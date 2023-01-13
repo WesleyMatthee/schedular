87e1c232680468c2a5da717b8088f190bdb89f70
Wesley Matthee <wrmatthee@gmail.com>
2023-01-13 18:47:32 -0500

    Added custom hook to index.js Appointment component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,19 +3,36 @@ import "components/Appointment/style.scss";
 import Show from "./Show";
 import Empty from "./Empty";
 import Header from "./Header";
+import Form from "./Form";
+import useVisualMode from "hooks/useVisualMode";
 // import { Fragment } from "react";
 
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 const Appointment = function(props) {
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
+
   return (
-  
+
+
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ? <Show 
-        student={props.interview.student} 
-        interviewer={props.interview.interviewer} //HARD CODED ID
-      /> : <Empty />}
-     
+      {mode === CREATE && (
+        <Form interviewers={[]} onCancel={back}/>
+      )}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
+
     </article>
 
   );
